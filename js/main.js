4ffa3767122f80238d5fce7515eeccdd01560db5
Jean Carlos Ademir Garcia Palomino <i201623068@cibertec.edu.pe>
2019-12-01 19:31:43 -0500

    Pagina de Prueba Subir a git

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+    // console.log("ya cargó todo");
+    let pantallaCarga = document.getElementById("pantallaCarga");
+    pantallaCarga.setAttribute("hidden", "hidden");
+}
