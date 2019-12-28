676d28972ffcb8c37137c4d57ecc01c053acdf98
Jean Carlos Ademir Garcia Palomino <i201623068@cibertec.edu.pe>
2019-12-28 11:46:12 -0500

    Cambios, Aplicando JQUERY
    Animacion y links, falta whatsap

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@ window.onload = () => {
     let pantallaCarga = document.getElementById("pantallaCarga");
     pantallaCarga.setAttribute("hidden", "hidden");
 }
+
+$("#linkquienes").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#quienessomos").offset().top
+    }, 1000);
+});
+
+$("#linkmision").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#mision").offset().top
+    }, 1000);
+});
+
+$("#linkvision").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#vision").offset().top
+    }, 1000);
+});
+
+$("#linkobjr").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#obrealizado").offset().top
+    }, 1000);
+});
