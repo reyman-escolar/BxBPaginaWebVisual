c78c6decd32fbe4cc7ad6256e2a3274ce74a2c28
Jean Carlos Ademir Garcia Palomino <i201623068@cibertec.edu.pe>
2020-01-02 23:33:54 -0500

    Agrego de Mapa, Ubicación.
    Falta mencionar la dirección del stand

---
+++ js/mapbox.js
@@ -0,0 +1,40 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicmV5bWFuMjAyIiwiYSI6ImNrNDI0bnIwNjA3cDQzaHJ3djBnNjgycjAifQ.OWrg7bBLW-b9a6ieTG5qLQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.5299519,-16.4012618], // starting position
+    zoom: 16 // starting zoom
+});
+map.on('load', function () {
+    map.loadImage(
+        './img/map-point.png',
+        function (error, image) {
+            if (error) throw error;
+            map.addImage('cat', image);
+            map.addLayer({
+                'id': 'points',
+                'type': 'symbol',
+                'source': {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'FeatureCollection',
+                        'features': [
+                            {
+                                'type': 'Feature',
+                                'geometry': {
+                                    'type': 'Point',
+                                    'coordinates': [-71.5299519, -16.4012618],
+                                }
+                            }
+                        ]
+                    }
+                },
+                'layout': {
+                    'icon-image': 'cat',
+                    'icon-size': 0.10
+                }
+            });
+        }
+    );
+});
+map.addControl(new mapboxgl.NavigationControl());
