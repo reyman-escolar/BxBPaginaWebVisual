2a29000f7b5d1196417a8f497ca21ca09659c7a0
reyman-escolar <52724397+reyman-escolar@users.noreply.github.com>
2020-02-10 14:44:16 -0500

    Listado para empaquetamiento

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import "./../css/all.min.css";
+import "./../css/bootstrap.css";
+import "./../css/bootstrap.min.css";
+import "./../css/estilos.css";
+import "./../css/font-awesome.min.css";
+import "./../css/imagenes.css";
+
+import  "./../index.html";
+import  "./../index.html";
+import  "./../index.html";
+
+import "./bootstrap";
+import "./bootstrap.min";
+import "./jquery";
+import "./jquery.min";
+import "./main";
+import "./mapbox";
+import "./popper";
+import "./popper.min";
